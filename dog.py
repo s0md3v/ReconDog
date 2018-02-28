77838df4950b5467429090da448119dd4cc6672a
rrampage <raunak1001@gmail.com>
2018-03-01 00:47:10 +0530

    Modified dog.py to run on Python 2 and 3

--- dog.py
+++ dog.py
@@ -1,151 +1,155 @@
 #!/usr/bin/env python
-import os
-from urllib2 import *
-from colorama import init, Fore, Back, Style
-init(convert=True)
+import sys
+
+VER = 2
+
+try:
+    if sys.version_info >= (3,0):
+        VER = 3
+        from urllib.request import urlopen
+        from urllib.error import URLError
+    else:
+        input = raw_input
+        from urllib2 import urlopen
+        from urllib2 import URLError
+except:
+        pass
+
+
+def fetch(url, decoding='utf-8'):
+    "Fetches content of URL"
+    return urlopen(url).read().decode(decoding)
 
 def banner():
-    print "\033[93m                               ___       ___\033[1;m"
-    print "\033[93m                              |   \_____/   |\033[1;m"
-    print "\033[93m                             /  |\/     \/|  \\    \033[1;m"
-    print "\033[93m                             \_/ | /\ /\ | \_/\033[1;m"
-    print "\033[97m ____  ____ _____ _____  __   _ \033[1;m \033[93m|_\/ \/_| \033[97m _____   ____   ____\033[1;m"
-    print "\033[97m|___/  |___ |     |    | | \  | \033[1;m\033[93m/   \o/   \ \033[97m|    \ |    | | ___\033[1;m"
-    print "\033[97m|   \_ |___ |____ |____| |  \_| \033[1;m\033[93m\___/'\___/ \033[97m|____/ |____| |____| v0.8\033[1;m"
-    print "\t\t\033[97m     Made with \033[1;m\033[1;31m<3\033[1;31m\033[97m By Team Ultimate\033[1;m"
+    print("\033[93m                               ___       ___\033[1;m")
+    print("\033[93m                              |   \_____/   |\033[1;m")
+    print("\033[93m                             /  |\/     \/|  \\    \033[1;m")
+    print("\033[93m                             \_/ | /\ /\ | \_/\033[1;m")
+    print("\033[97m ____  ____ _____ _____  __   _ \033[1;m \033[93m|_\/ \/_| \033[97m _____   ____   ____\033[1;m")
+    print("\033[97m|___/  |___ |     |    | | \  | \033[1;m\033[93m/   \o/   \ \033[97m|    \ |    | | ___\033[1;m")
+    print("\033[97m|   \_ |___ |____ |____| |  \_| \033[1;m\033[93m\___/'\___/ \033[97m|____/ |____| |____| v0.8\033[1;m")
+    print("\t\t\033[97m     Made with \033[1;m\033[1;31m<3\033[1;31m\033[97m By Team Ultimate\033[1;m")
+
 def menu():
-    print "\n\033[97m1. Whois Lookup\033[1;m"
-    print "\033[97m2. DNS Lookup + Cloudflare Detector\033[1;m"
-    print "\033[97m3. Zone Transfer\033[1;m"
-    print "\033[97m4. Port Scan\033[1;m"
-    print "\033[97m5. HTTP Header Grabber\033[1;m"
-    print "\033[97m6. Honeypot Detector\033[1;m"
-    print "\033[97m7. Robots.txt Scanner\033[1;m"
-    print "\033[97m8. Link Grabber\033[1;m"
-    print "\033[97m9. IP Location Finder\033[1;m"
-    print "\033[97m10. Traceroute\033[1;m"
-    print "\033[97m11. Exit\033[1;m"
+    print("\n\033[97m1. Whois Lookup\033[1;m")
+    print("\033[97m2. DNS Lookup + Cloudflare Detector\033[1;m")
+    print("\033[97m3. Zone Transfer\033[1;m")
+    print("\033[97m4. Port Scan\033[1;m")
+    print("\033[97m5. HTTP Header Grabber\033[1;m")
+    print("\033[97m6. Honeypot Detector\033[1;m")
+    print("\033[97m7. Robots.txt Scanner\033[1;m")
+    print("\033[97m8. Link Grabber\033[1;m")
+    print("\033[97m9. IP Location Finder\033[1;m")
+    print("\033[97m10. Traceroute\033[1;m")
+    print("\033[97m11. Exit\033[1;m")
+
 def dog():
-    try:
+    choice = '1'         # Set as default to enter in the loop
+    banner()
+
+    while choice != '11':
+        menu()
         choice = input('\033[1;91mEnter your choice:\033[1;m ')
-        if choice == 1:
-            domip = raw_input('\033[1;91mEnter Domain or IP Address: \033[1;m')
+
+        if choice == '1':
+            domip = input('\033[1;91mEnter Domain or IP Address: \033[1;m')
             who = "http://api.hackertarget.com/whois/?q=" + domip
-            pwho = urlopen(who).read()
-            print (pwho)
-            menu()
-            dog()
-        if choice == 2:
-            domain = raw_input('\033[1;91mEnter Domain: \033[1;m')
+            pwho = fetch(who)
+            print(pwho)
+
+        elif choice == '2':
+            domain = input('\033[1;91mEnter Domain: \033[1;m')
             ns = "http://api.hackertarget.com/dnslookup/?q=" + domain
-            pns = urlopen(ns).read()
-            print (pns)
+            pns = fetch(ns)
+            print(pns)
+
             if 'cloudflare' in pns:
-                print "\033[1;31mCloudflare Detected!\033[1;m"
+                print("\033[1;31mCloudflare Detected!\033[1;m")
             else:
-                print "\033[1;31mNot Protected By cloudflare\033[1;m"
-            menu()
-            dog()
-        if choice == 3:
-            domain = raw_input('\033[1;91mEnter Domain: \033[1;m')
+                print("\033[1;31mNot Protected By cloudflare\033[1;m")
+
+        elif choice == '3':
+            domain = input('\033[1;91mEnter Domain: \033[1;m')
             zone = "http://api.hackertarget.com/zonetransfer/?q=" + domain
-            try:
-                domain = raw_input('\033[1;91mEnter Domain: \033[1;m')
-                pzone = urlopen(zone).read()
-                print (pzone)
-            except 'failed' in pzone:
-                print "\033[1;31mZone transfer failed\033[1;m"
-            menu()
-            dog()
-        if choice == 4:
-            domip = raw_input('\033[1;91mEnter Domain or IP Address: \033[1;m')
+            pzone = fetch(zone)
+            print(pzone)
+            if 'failed' in pzone:
+                print("\033[1;31mZone transfer failed\033[1;m")
+
+        elif choice == '4':
+            domip = input('\033[1;91mEnter Domain or IP Address: \033[1;m')
             port = "http://api.hackertarget.com/nmap/?q=" + domip
-            pport = urlopen(port).read()
+            pport = fetch(port)
             print (pport)
-            menu()
-            dog()
-        if choice == 5:
-            domip = raw_input('\033[1;91mEnter Domain or IP Address: \033[1;m')
+
+        elif choice == '5':
+            domip = input('\033[1;91mEnter Domain or IP Address: \033[1;m')
             header = "http://api.hackertarget.com/httpheaders/?q=" + domip
-            pheader = urlopen(header).read()
-            print (pheader)
-            menu()
-            dog()
-        if choice == 6:
-            ip = raw_input('\033[1;91mEnter IP Address: \033[1;m')
+            pheader = fetch(header)
+            print(pheader)
+
+        elif choice == '6':
+            ip = input('\033[1;91mEnter IP Address: \033[1;m')
             honey = "https://api.shodan.io/labs/honeyscore/" + ip + "?key=C23OXE0bVMrul2YeqcL7zxb6jZ4pj2by"
-            phoney = urlopen(honey).read()
-            if '0.0' in phoney:
-                print "\033[1;32mHoneypot Probabilty: 0%\033[1;m"
-            if '0.1' in phoney:
-                print "\033[1;32mHoneypot Probabilty: 10%\033[1;m"
-            if '0.2' in phoney:
-                print "\033[1;32mHoneypot Probabilty: 20%\033[1;m"
-            if '0.3' in phoney:
-                print "\033[1;32mHoneypot Probabilty: 30%\033[1;m"
-            if '0.4' in phoney:
-                print "\033[1;32mHoneypot Probabilty: 40%\033[1;m"
-            if '0.5' in phoney:
-                print "\033[1;31mHoneypot Probabilty: 50%\033[1;m"
-            if '0.6' in phoney:
-                print "\033[1;31mHoneypot Probabilty: 60%\033[1;m"
-            if '0.7' in phoney:
-                print "\033[1;31mHoneypot Probabilty: 70%\033[1;m"
-            if '0.8' in phoney:
-                print "\033[1;31mHoneypot Probabilty: 80%\033[1;m"
-            if '0.9' in phoney:
-                print "\033[1;31mHoneypot Probabilty: 90%\033[1;m"
-            if '1.0' in phoney:
-                print "\033[1;31mHoneypot Probabilty: 100%\033[1;m"
-            menu()
-            dog()
-        if choice == 7:
-            domain = raw_input('\033[1;91mEnter Domain: \033[1;m')
-            if 'http://' in domain or 'https://' in domain:
-                pass
-            else:
+            
+            try:
+                phoney = fetch(honey)
+            except URLError:
+                phoney = None
+                print('\033[1;31m[-] No information available for that IP!\033[1;m')
+            
+            if phoney:
+                print('\033[1;3{color}mHoneypot Probabilty: {probability}%\033[1;m'.format(color='2' if float(phoney) < 0.5 else '3', probability=float(phoney) * 10))
+ 
+
+        elif choice == '7':
+            domain = input('\033[1;91mEnter Domain: \033[1;m')
+
+            if not (domain.startswith('http://') or domain.startswith('https://')):
                 domain = 'http://' + domain
             robot = domain + "/robots.txt"
-            probot = urlopen(robot).read()
-            print (probot)
-            menu()
-            dog()
-        if choice == 8:
-            page = raw_input('\033[1;91mEnter URL: \033[1;m')
-            if 'http://' in page or 'https://' in page:
-                pass
-            else:
+            
+            try:
+                probot = fetch(robot)
+                print(probot)
+            except URLError:
+                print('\033[1;31m[-] Can\'t access to {page}!\033[1;m'.format(page=robot))        
+
+        elif choice == '8':
+            page = input('\033[1;91mEnter URL: \033[1;m')
+
+            if not (page.startswith('http://') or page.startswith('https://')):
                 page = 'http://' + page
             crawl = "https://api.hackertarget.com/pagelinks/?q=" + page
-            pcrawl = urlopen(crawl).read()
+            pcrawl = fetch(crawl)
             print (pcrawl)
-            menu()
-            dog()
-        if choice == 9:
-            ip = raw_input('\033[1;91mEnter IP Address: \033[1;m')
+
+        elif choice == '9':
+            ip = input('\033[1;91mEnter IP Address: \033[1;m')
             geo = "http://ipinfo.io/" + ip + "/geo"
-            pgeo = urlopen(geo).read()
-            print (pgeo)
-            menu()
-            dog()
-        if choice == 10:
-            domip = raw_input('\033[1;91mEnter Domain or IP Address: \033[1;m')
+            
+            try:
+                pgeo = fetch(geo)
+                print(pgeo)
+            except URLError:
+                print('\033[1;31m[-] Please provide a valid IP address!\033[1;m')
+
+        elif choice == '10':
+            domip = input('\033[1;91mEnter Domain or IP Address: \033[1;m')
             trace = "https://api.hackertarget.com/mtr/?q=" + domip
-            ptrace = urlopen(trace).read()
+            ptrace = fetch(trace)
             print (ptrace)
-            menu()
-            dog()
-        if choice == 11:
-            print "\033[97m11. Exiting\033[1;m"
-            os._exit(1)
+
+        elif choice == '11':
+            print('\033[97m11. Exiting\033[1;m')
+
         else:
-            print "\033[1;31m[-] Invalid option!\033[1;m"
-            menu()
-            dog()
-    except:
-        print "\033[1;31m[-] Something went wrong!\033[1;m"
-        menu()
-        dog()
-banner()
-menu()
-dog()
+            print('\033[1;31m[-] Invalid option!\033[1;m')
+        #except:
+        #    print('\033[1;31m[-] Something wrong happened!\033[1;m')
+
+
+#=====# Main #=====#
+
+if __name__ == '__main__':
+    dog()
