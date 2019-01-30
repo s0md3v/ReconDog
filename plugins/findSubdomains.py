7ea2c7100f5a582b9b8dc3e1897517ffc8f51bb6
Somdev Sangwan <s0md3v@gmail.com>
2019-01-30 16:12:03 +0530

    fixed subdomain enumeration

--- plugins/findSubdomains.py
+++ plugins/findSubdomains.py
@@ -1,11 +1,10 @@
 import sys
 from re import findall
-from tld import get_tld
 from requests import get
 
 def findSubdomains(host):
     response = get('https://findsubdomains.com/subdomains-of/' +
-                   get_tld(host, fix_protocol=True)).text
+                   host).text
     matches = findall(r'(?s)<div class="domains js-domain-name">(.*?)</div>', response)
     for match in matches:
         sys.stdout.write(match.replace(' ', '').replace('\n', '') + '\n')
