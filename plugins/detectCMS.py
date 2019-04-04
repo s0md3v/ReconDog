9e1a5fc6cdd18c591a59905436f5de784ca06f52
Somdev Sangwan <s0md3v@gmail.com>
2019-04-04 12:47:22 +0530

    Fixes #26 (CMS detection)

--- plugins/detectCMS.py
+++ plugins/detectCMS.py
@@ -5,9 +5,9 @@ from core.colors import info, good
 
 
 def detectCMS(domain):
-    response = get('https://whatcms.org/?s=%s' % domain).text
-    match = search(r'<a href="/c/\w+" class="nowrap" title="\w+">(.*?)</a></div>', response)
+    response = get('https://whatcms.org/?gpreq=json&jsoncallback=jQuery1124008091494457806547_1554361369057&s=%s&na=&nb=1cg805dlm7d7e5eickf67rzxrn12mju6bnch3a99hrt88v7n8rhf0lovwr8d0zm1&verified=&_=1554361369059' % domain).text
+    match = search(r'uses<\\/div>[^>]+>(.*?)<\\/a>', response)
     try:
-        sys.stdout.write(good + ' ' + match.group(1).split('</a>')[0] + '\n')
+        sys.stdout.write(good + ' ' + match.group(1) + '\n')
     except:
         sys.stdout.write('%s Target doesn\'t seem to use a CMS' % info + '\n')
